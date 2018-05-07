5a23386f7c29c7d58b30e7c48ebdc3ae8f20bbd9
Massaki Archambault <badjware@massaki.ca>
2018-05-07 17:11:21 -0400

    set colorscheme to spacegrey, font to Hack and font-size to 15

--- html/js/app.js
+++ html/js/app.js
@@ -247,28 +247,28 @@ var openWs = function() {
         }
 
         term = new Terminal({
-            fontSize: 13,
-            fontFamily: '"Menlo for Powerline", Menlo, Consolas, "Liberation Mono", Courier, monospace',
+            fontSize: 15,
+            fontFamily: '"Knack Nerd Font", Hack, Consolas, "Liberation Mono", Courier, monospace',
             theme: {
-                foreground: '#d2d2d2',
-                background: '#2b2b2b',
+                foreground: '#B7BBB7',
+                background: '#1C1C1C',
                 cursor: '#adadad',
-                black: '#000000',
-                red: '#d81e00',
-                green: '#5ea702',
-                yellow: '#cfae00',
-                blue: '#427ab3',
-                magenta: '#89658e',
-                cyan: '#00a7aa',
-                white: '#dbded8',
-                brightBlack: '#686a66',
-                brightRed: '#f54235',
-                brightGreen: '#99e343',
-                brightYellow: '#fdeb61',
-                brightBlue: '#84b0d8',
-                brightMagenta: '#bc94b7',
-                brightCyan: '#37e6e8',
-                brightWhite: '#f1f1f0'
+                black: '#2C2F33',
+                red: '#B04C50',
+                green: '#919652',
+                yellow: '#E2995C',
+                blue: '#66899D',
+                magenta: '#8D6494',
+                cyan: '#527C77',
+                white: '#606360',
+                brightBlack: '#4B5056',
+                brightRed: '#B04C50',
+                brightGreen: '#919652',
+                brightYellow: '#E2995C',
+                brightBlue: '#66899D',
+                brightMagenta: '#8D6494',
+                brightCyan: '#527C77',
+                brightWhite: '#DDE3DC'
             }
         });
 
